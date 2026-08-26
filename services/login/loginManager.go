4310d04ab0cefd5d66934de36515fa8f257fcac2
maciej <maciekursyn@gmail.com>
2026-08-26 11:38:15 +0200

    split login manager from login handler

---
+++ services/login/loginManager.go
@@ -0,0 +1,23 @@
+package login
+
+import (
+	"context"
+	"net/http"
+	"revit/internal/permissions"
+	"revit/services/auth"
+)
+
+type LoginManager struct {
+	authService    auth.AuthManager
+	store          loginStore
+	permissionsMgr permissions.PermissionsManager
+}
+
+func NewLoginManager(ctx context.Context, queries loginStore, authService auth.AuthManager, permissionMgr permissions.PermissionsManager) (*LoginManager, error) {
+	return &LoginManager{authService: authService, store: queries, permissionsMgr: permissionMgr}, nil
+}
+
+func (l *LoginManager) RegisterRoutes(mux *http.ServeMux) {
+	mux.HandleFunc("POST /session/login", l.LoginHandler())
+	mux.HandleFunc("GET /session/logout", l.LogoutHandler())
+}
