d1797c4521303c165f35382d57e6930ffa1140a0
maciej <maciekursyn@gmail.com>
2026-09-01 22:58:57 +0200

    Split-up testing of the submission service.

---
+++ services/submission/constants_test.go
@@ -0,0 +1,17 @@
+package submission
+
+import (
+	"revit/internal/db"
+	services "revit/internal/testingservices"
+)
+
+var DEFAULT_USERDATA = UserData{Email: services.DEFAULT_EMAIL, Name: services.DEFAULT_USERNAME}
+var DEFAULT_RESP_SUBMISSION_1 = Submission{Content: "a", Author: DEFAULT_USERDATA, Hash: "a"}
+var DEFAULT_RESP_SUBMISSION_2 = Submission{Content: "b", Author: DEFAULT_USERDATA, Hash: "b"}
+var DEFAULT_RESP_SUBMISSIONS = []Submission{DEFAULT_RESP_SUBMISSION_1, DEFAULT_RESP_SUBMISSION_2}
+
+var DEFAULT_SUBMISSION_1 = db.Submission{ID: 1, Content: "a", Author: 1, Hash: "a"}
+var DEFAULT_SUBMISSION_2 = db.Submission{ID: 2, Content: "b", Author: 1, Hash: "b"}
+var DEFAULT_SUBMISSIONS = []db.Submission{DEFAULT_SUBMISSION_1, DEFAULT_SUBMISSION_2}
+
+const TEST_SUBMISSION = "Lorem Ipsum"
